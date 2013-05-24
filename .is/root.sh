6c9092bc260766bbd7c06f69b569d0e10cfab887
Max Maximov <max.maximov@gmail.com>
2013-05-24 20:40:18 +0400

    ttt

--- .is/root.sh
+++ .is/root.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # wget -O - https://raw.github.com/maxmaximov/rc/master/.is/root.sh | sh
 useradd -m -G sudo maxmaximov
-su -cl maxmaximov "wget -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh"
+su -lc maxmaximov "wget -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh"
