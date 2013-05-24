e89930cb65d104db4fc074538571f572b6bd4e42
Max Maximov <max.maximov@gmail.com>
2013-05-24 20:19:37 +0400

    is

---
+++ is/.common.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# wget -O - https://raw.github.com/maxmaximov/rc/master/is/.common.sh | sh
+cd
+mkdir -p .ssh
+chmod 700 .ssh
+wget -O - https://raw.github.com/maxmaximov/rc/master/.ssh/id_rsa.pub >> .ssh/authorized_keys
+chmod 600 .ssh/*
+git clone git@github.com:maxmaximov/rc.git
+mv rc/.git .
+rm -rf rc
+git checkout .
+cd -
+chsh -s /bin/zsh
+zsh
