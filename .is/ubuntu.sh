19a6abc582bbdd808af20ed4ac13bfe54c0db2d7
Max Maximov <max.maximov@gmail.com>
2013-05-25 21:32:02 +0400

    ubunte node.js

--- .is/ubuntu.sh
+++ .is/ubuntu.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh
-sudo apt-get --yes install colordiff vim zsh git nodejs npm
+sudo apt-get update
+sudo apt-get --yes install python3-software-properties
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get --yes install colordiff vim zsh git nodejs
 wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/common.sh | sh
