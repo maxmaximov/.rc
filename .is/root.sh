b32602c38654d680ca70ec660dd5e96c3e787860
Max Maximov <max.maximov@gmail.com>
2013-05-25 21:52:23 +0400

    root.sh, ubuntu.sh

--- .is/root.sh
+++ .is/root.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ssh root@host "wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/root.sh | sh"
-useradd -mG maxmaximov
+useradd -m maxmaximov
 echo -e "maxmaximov\tALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ssh-add
 wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh
