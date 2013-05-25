64081a6b6759b79ad193ab132acbe1e6b0bdebbc
Max Maximov <max.maximov@gmail.com>
2013-05-25 21:49:18 +0400

    root.sh, ubuntu.sh

--- .is/root.sh
+++ .is/root.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # ssh root@host "wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/root.sh | sh"
-useradd -mG sudo maxmaximov
+useradd -mG maxmaximov
+echo -e "maxmaximov\tALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 ssh-add
 wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh
--- .is/ubuntu.sh
+++ .is/ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/ubuntu.sh | sh
 sudo apt-get update
-sudo apt-get --yes install python3-software-properties
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get --yes install software-properties-common
+sudo add-apt-repository --yes ppa:chris-lea/node.js
 sudo apt-get update
-sudo apt-get --yes install colordiff vim zsh git nodejs
+sudo apt-get --yes install colordiff vim zsh git nodejs npm
 wget -nv -O - https://raw.github.com/maxmaximov/rc/master/.is/common.sh | sh
