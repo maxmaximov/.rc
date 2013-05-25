539d77fea2a26cf78c78bacdcfef118c0e7ab455
Max Maximov <max.maximov@gmail.com>
2013-05-25 05:12:03 +0400

    is

--- .is/common.sh
+++ .is/common.sh
@@ -5,6 +5,7 @@ mkdir -p .ssh
 chmod 700 .ssh
 wget -O - https://raw.github.com/maxmaximov/rc/master/.ssh/id_rsa.pub >> .ssh/authorized_keys
 chmod 600 .ssh/*
+ssh -o "StrictHostKeyChecking no" git@github.com
 git clone git@github.com:maxmaximov/rc.git
 mv rc/.git .
 rm -rf rc
